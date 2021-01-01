747477b3300867ebb17a49091bda5a08955201be
VinceWangw <1354329221@qq.com>
2021-01-01 23:42:06 +0800

    2021 加油!

---
+++ src/day19/基本计算器II/Solution.java
@@ -0,0 +1,102 @@
+package day19.基本计算器II;
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 实现一个基本的计算器来计算一个简单的字符串表达式的值。
+ * <p>
+ * 字符串表达式仅包含非负整数，+， - ，*，/ 四种运算符和空格 。 整数除法仅保留整数部分。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "3+2*2"
+ * 输出: 7
+ * 示例 2:
+ * <p>
+ * 输入: " 3/2 "
+ * 输出: 1
+ * 示例 3:
+ * <p>
+ * 输入: " 3+5 / 2 "
+ * 输出: 5
+ */
+class Solution {
+
+    public int calculate(String s) {
+
+        s = s.replace(" ", "");
+
+        Deque<String> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); ) {
+
+            if (Character.isDigit(s.charAt(i))){
+                int num = 0;
+                while (i < s.length() && Character.isDigit(s.charAt(i))){
+                    num = num * 10 + (s.charAt(i) - '0');
+                    i ++;
+                }
+                stack.push(String.valueOf(num));
+            }else if (s.charAt(i) == '+'){
+                stack.push("+");
+                i ++;
+            }else if (s.charAt(i) == '-'){
+                stack.push("+");
+                i ++;
+                int num = 0;
+                while (i < s.length() && Character.isDigit(s.charAt(i))){
+                    num = num * 10 + (s.charAt(i) - '0');
+                    i ++;
+                }
+                stack.push(Integer.toString(-num));
+            }else if (s.charAt(i) == '*'){
+                int a = Integer.parseInt(stack.pop());
+                i ++;
+                int num = 0;
+                while (i < s.length() && Character.isDigit(s.charAt(i))){
+                    num = num * 10 + (s.charAt(i) - '0');
+                    i ++;
+                }
+                stack.push(String.valueOf(a * num));
+            }else if (s.charAt(i) == '/'){
+                int a = Integer.parseInt(stack.pop());
+                i ++;
+                int num = 0;
+                while (i < s.length() && Character.isDigit(s.charAt(i))){
+                    num = num * 10 + (s.charAt(i) - '0');
+                    i ++;
+                }
+                if (num == 0) {
+                    throw new IllegalArgumentException("除数不能为0");
+                }
+                int r = a / num;
+                stack.push(String.valueOf(r));
+            }
+        }
+
+        while (stack.size() > 1) {
+            int a = Integer.parseInt(stack.pop());
+            char c = stack.poll().charAt(0);
+            int b = Integer.parseInt(stack.pop());
+            if (c == '+') {
+                stack.push(String.valueOf(a+b));
+            }
+        }
+        return Integer.parseInt(stack.pop());
+    }
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().calculate("1-1+1"));
+        System.out.println(new Solution().calculate("3+5 / 2"));
+        System.out.println(new Solution().calculate("3 + 6"));
+        System.out.println(new Solution().calculate("5/2"));
+        System.out.println(new Solution().calculate("3+ 5/2* 2-2"));
+        System.out.println(new Solution().calculate("2-1"));
+        System.out.println(new Solution().calculate("42"));
+        System.out.println(new Solution().calculate("1*2-3/4+5*6-7*8+9/10"));
+    }
+}
---
+++ src/day19/种花问题/Solution.java
@@ -0,0 +1,52 @@
+package day19.种花问题;
+
+
+/**
+ * 假设你有一个很长的花坛，一部分地块种植了花，另一部分却没有。
+ * 可是，花卉不能种植在相邻的地块上，它们会争夺水源，两者都会死去。
+ * <p>
+ * 给定一个花坛（表示为一个数组包含0和1，其中0表示没种植花，1表示种植了花），和一个数n。
+ * 能否在不打破种植规则的情况下种入 n 朵花？能则返回True，不能则返回False。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: flowerbed = [1,0,0,0,1], n = 1
+ * 输出: True
+ * 示例 2:
+ * <p>
+ * 输入: flowerbed = [1,0,0,0,1], n = 2
+ * 输出: False
+ */
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+        int len = flowerbed.length;
+        int i = -1;
+        while (i < len) {
+            if (i < 0 || flowerbed[i] == 0) {
+                int j = i + 1;
+                int k = i + 2;
+                if (j < len && flowerbed[j] == 0){
+                    if (k >= len || flowerbed[k] == 0){
+                        n --;
+                    }
+                    i = j;
+                }
+            }
+            i ++;
+        }
+
+        return n <= 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,0,0,0,1};
+        System.out.println(new Solution().canPlaceFlowers(nums,1));
+        System.out.println(new Solution().canPlaceFlowers(nums,2));
+        System.out.println(new Solution().canPlaceFlowers(new int[]{1,0,0,0,0,0,1},2));
+        System.out.println(new Solution().canPlaceFlowers(new int[]{0,0,0},2));
+        System.out.println(new Solution().canPlaceFlowers(new int[]{0,0},2));
+        System.out.println(new Solution().canPlaceFlowers(new int[]{0},1));
+        System.out.println(new Solution().canPlaceFlowers(new int[]{0,0},1));
+    }
+}
